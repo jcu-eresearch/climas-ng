3d80ba05a6d615eeeb955c8c4a084394d4f7e698
Daniel Baird <daniel@danielbaird.com>
2014-10-15 11:53:59 +1000

    fixed paths for JSON list maker

--- utilityscripts/createdatalists.py
+++ utilityscripts/createdatalists.py
@@ -4,12 +4,12 @@
 # .../climasng/data/ directory.
 
 ## local dev paths
-jsondir = '/Users/pvrdwb/jcu/cng/webapp/climasng/data'
-datadir = '/Volumes/DanielsDisk/work/CliMAS-NG/datasubset'
+# jsondir = '/Users/pvrdwb/jcu/cng/webapp/climasng/data'
+# datadir = '/Volumes/DanielsDisk/work/CliMAS-NG/datasubset'
 
 ## HPC / prod paths
-# jsondir = '/var/climaswebapp/climasng/data'
-# datadir = '/rdsi/climas/taxa-test'
+jsondir = '/var/climaswebapp/climasng/data'
+datadir = '/rdsi/climas/taxa-test'
 
 import sys
 sys.path.append(jsondir)
