424416cc3bbc9a07f4dde01ca878ba0d42a43eaa
Daniel Baird <daniel@danielbaird.com>
2017-07-17 14:09:23 +1000

    testing species scraper

--- utilityscripts/createdatalists.py
+++ utilityscripts/createdatalists.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
 # run this to create species.json and biodiversity.json in the
-# .../climasng/data/ directory.
-
-## local dev paths
-# jsondir = '/Users/pvrdwb/jcu/cng/webapp/climasng/data'
-# datadir = '/Volumes/DanielsDisk/work/CliMAS-NG/datasubset'
+# webapp's static/data directory.
 
 ## HPC / prod paths
-jsondir = '/var/climaswebapp/climasng/data'
-datadir = '/rdsi/climas/taxa'
+jsondir = '/var/wallacewebapp/climasng/static/data'
+datadir = '/rdsi/vol08/wallace2/W2_website'
 
+if os.path.isdir('/Users/pvrdwb'):
+
+	## local dev paths
+	jsondir = '/Users/pvrdwb/projects/climas-global/webapp/climasng/static/data'
+	datadir = '/Users/pvrdwb/projects/climas-global/testdata'
+
+#################################################
+
+import os
 import sys
-sys.path.append(jsondir)
+sys.path.append('.')
 import datafinder
 
-datafinder.createSpeciesJson(datadir)
-datafinder.createBiodiversityJson(datadir)
+datafinder.createSpeciesJson(datadir, os.path.join(jsondir, 'species.json'))
+# datafinder.createBiodiversityJson(datadir)
---
+++ utilityscripts/datafinder.py
@@ -0,0 +1,118 @@
+
+import os
+import re
+import json
+
+def createSpeciesJson(source_path, output_file):
+    # create the species.json file using data from the specified path
+
+    # traverse directories looking for dirs named "1km".  If it's
+    # path matches this pattern:
+    # .../<taxon-name>/models/<species-name>/1km
+    # then record that as a species / taxon record.
+
+    # here's a regex to test for species dirs:
+
+    #                group(8) - Species --------------------------------------------.
+    #                           (literal underscore) ----------------------------.  |
+    #                group(7) - Genus ----------------------------------------.  |  |
+    #                group(6) - Genus, again ---------------------------.     |  |  |
+    #                group(5) - Family ---------------------------.     |     |  |  |
+    #                group(4) - Order ----------------------.     |     |     |  |  |
+    #                group(3) - Class ----------------.     |     |     |     |  |  |
+    #                group(2) - Phylum ---------.     |     |     |     |     |  |  |
+    #                group(1) - Kingdom --.     |     |     |     |     |     |  |  |
+    #                                     V     V     V     V     V     V     V  V  V
+    sppdir_regex = re.compile(r'species/(\w+)/(\w+)/(\w+)/(\w+)/(\w+)/(\w+)/(\w+)_(\w+)/summaries_temperature$')
+
+    # for example:
+    #  Species -----------------------------------------------------------.
+    #  Genus ----------------------------------------------------.        |
+    #  Genus, again --------------------------------------.      |        |
+    #  Family -------------------------------------.      |      |        |
+    #  Order ------------------------------.       |      |      |        |
+    #  Class ----------------------.       |       |      |      |        |
+    #  Phylum ------------.        |       |       |      |      |        |
+    #  Kingdom --.        |        |       |       |      |      |        |
+    #            V        V        V       V       V      V      V        V
+    # species/Animalia/Chordata/Amphibia/Anura/Alytidae/Alytes/Alytes_cisternasii/summaries_temperature/
+
+
+    # we'll get the species common name from here:
+    common_names = {}
+
+    # cn_file = os.path.join(os.path.dirname(__file__), 'all_species.json')
+    # try:
+    #     # try reading in the list of sci-to-common species names
+    #     with open(cn_file) as f:
+    #         common_names = json.load(f)
+    # except:
+    #     # give up on common names if we can't read them
+    #     common_names = {}
+
+
+    #
+    # okay, ready to check for modelled species
+    #
+    species_list = {}
+    for dir, subdirs, files in os.walk(source_path):
+
+        match = sppdir_regex.search(dir)
+
+        if match:
+            sci_class = match.group(1)
+            sci_name = match.group(7) + ' ' + match.group(8)
+            species_list[sci_name] = {
+                "commonNames": common_names.get(sci_name, [""]),
+                "group": sci_class
+            }
+
+            # if we found a species dir, we don't need to keep
+            # os.walk()ing into its descendent dirs
+            subdirs[:] = []
+
+    # now save our species list
+    with open(output_file, 'w') as json_file:
+        json.dump(species_list, json_file, sort_keys = True, indent = 4)
+
+
+def createBiodiversityJson(source_path):
+    # create the biodiversity.json file using data from the specified path
+
+    # traverse directories looking for "deciles" dirs.
+    # If a dir's path matches this pattern:
+    # .../<taxon-name>/biodiversity/deciles
+    # then record that as a taxon / biodiversity record.
+
+    # here's a regex to test for biodiv dirs:
+    biodiv_regex = re.compile(r'/(\w+)/biodiversity/deciles$')
+
+    biodiv_list = {}
+    for dir, subdirs, files in os.walk(source_path):
+
+        match = biodiv_regex.search(dir)
+
+        if match:
+            taxon = match.group(1)
+            biodiv_list[taxon] = {
+                "group": taxon
+            }
+
+            # if we found a biodiv dir, we don't need to keep
+            # os.walk()ing into its descendent dirs
+            subdirs[:] = []
+
+    # now save our biodiv list
+    json_path = os.path.join(os.path.dirname(__file__), 'biodiversity.json')
+    with open(json_path, 'w') as json_file:
+        json.dump(biodiv_list, json_file, sort_keys = True, indent = 4)
+
+
+
+
+
+
+
+
+
+
