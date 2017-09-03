1145848e4386ada645f2452ccacbda9415d85162
Vlad Dracula <vlad@tran.sylvan.ia>
2017-09-04 09:56:32 +1000

    removing unused db

--- webapp/climasng/__init__.py
+++ webapp/climasng/__init__.py
@@ -2,10 +2,9 @@
 from pyramid.config import Configurator
 from pyramid.httpexceptions import HTTPNotFound
 
-from sqlalchemy import engine_from_config
+# from sqlalchemy import engine_from_config
 
-import sqlite3
-
+# import sqlite3
 
 from whoosh.fields import Schema, TEXT, NGRAM, NGRAMWORDS, ID, STORED, KEYWORD
 from whoosh import index
@@ -14,10 +13,10 @@ from whoosh.query import Or, And, Term
 
 # import pprint
 
-from .models import (
-    DBSession,
-    Base,
-    )
+# from .models import (
+#     DBSession,
+#     Base,
+#     )
 
 def notfound(request):
     # set the 404 page up here
@@ -26,14 +25,14 @@ def notfound(request):
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
+    # engine = engine_from_config(settings, 'sqlalchemy.')
+    # DBSession.configure(bind=engine)
+    # Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
 
     # now get all the table reflection done
-    Base.prepare(engine)
+    # Base.prepare(engine)
 
     # pre-cook the whoosh interface for the search api
     config.registry.settings['whoosh_index'] = index.open_dir('/var/wallacewebapp/climasng/data/searchindex')
@@ -42,7 +41,7 @@ def main(global_config, **settings):
 
     config.add_static_view('static', 'climasng:static/', cache_max_age=3600)
 
-    config.add_route('test', '/test/')
+    # config.add_route('test', '/test/')
 
     config.add_route('home', '/')
     config.add_route('science', '/science/')
@@ -50,17 +49,17 @@ def main(global_config, **settings):
 
     config.add_route('maps', '/maps/')
 
-    config.add_route('reports', '/reports/')
-    config.add_route('regionreport', '/regionreport/')
+    # config.add_route('reports', '/reports/')
+    # config.add_route('regionreport', '/regionreport/')
 
     config.add_route('data', '/data/{data_name}/')
     config.add_route('doc', '/info/{doc_name}/')
 
-    config.add_route('oldreports', '/oldreports/')
+    # config.add_route('oldreports', '/oldreports/')
 
-    config.add_route('oldmap', '/oldmap/')
-    config.add_route('oldspecies', '/oldspecies/{region}/{year}/speciestables.html')
-    config.add_route('reflector', '/reflector/')
+    # config.add_route('oldmap', '/oldmap/')
+    # config.add_route('oldspecies', '/oldspecies/{region}/{year}/speciestables.html')
+    # config.add_route('reflector', '/reflector/')
 
     config.add_route('speciesdata', '/speciesdata/{data_name:.*}')
     config.add_route('climatedata', '/climatedata/{data_name:.*}')
@@ -68,7 +67,7 @@ def main(global_config, **settings):
 
     config.add_route('regiondata', '/regiondata/{regiontype}/{regionid}')
 
-    config.add_static_view(name='olddata/regions', path=settings['climas.old_report_data_path'])
+    # config.add_static_view(name='olddata/regions', path=settings['climas.old_report_data_path'])
 
     # add a 404 view that will retry with an appended slash first
     config.add_notfound_view(notfound, append_slash=True)
